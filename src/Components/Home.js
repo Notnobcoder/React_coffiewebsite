22c83208c0602153b702acea9d454836d85d740a
tushar <tushar@lovoj.com>
2024-02-10 18:41:55 +0530

    new

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import "./component.css"
+import React from "react";
+import "./component.css";
 
 export const Home = () => {
   return (
-    <div className='HomePage'
-    intial={{opacity:0}}
-    animate={{opacity:1}}
-    exit={{opacity:0}}
+    <div
+      className="HomePage"
+      intial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
       <div className="HomeItems">
-          <h1 className='HomeHeading'>Coffie and Cookies</h1>
-          <p className="HomeHeading2">Have a treat of taste ..</p>
-          <button className='HomeButton'>Contact us</button>
+        <h1 className="HomeHeading">Coffie and Cookies</h1>
+        <p className="HomeHeading2">Have a treat of taste ..</p>
+        <button className="HomeButton">Contact us</button>
       </div>
     </div>
-  )
-}
+  );
+};
